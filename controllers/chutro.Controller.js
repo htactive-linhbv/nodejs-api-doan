2d89fdc60f14b06676c3187c81c981a4eba413d4
BuiVanLinh <linhbv@htacive.com>
2020-05-18 17:31:20 +0700

    'fix-host'

--- controllers/chutro.Controller.js
+++ controllers/chutro.Controller.js
@@ -312,7 +312,7 @@ module.exports = {
                                               <w:anchorlock/>
                                               <center>
                                             <![endif]-->
-                                                <a href="khutro.xyz/quantri/dichvu"
+                                                <a href="http://khutro.xyz/quantri/dichvu"
                                           style="background-color:#178f8f;border-radius:4px;color:#ffffff;display:inline-block;font-family:Helvetica, Arial, sans-serif;font-size:16px;font-weight:bold;line-height:50px;text-align:center;text-decoration:none;width:200px;-webkit-text-size-adjust:none;">Truy cập trang Quản trị</a>
                                             <!--[if mso]>
                                               </center>
